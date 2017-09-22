14a7a5720818800d1755214d91c639c43239d2cf
edwin <edwin.lzh@gmail.com>
2017-09-22 17:37:54 +0800

    init

---
+++ cmd/message.go
@@ -0,0 +1,90 @@
+// Copyright © 2017 edwin <edwin.lzh@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	log "qiniupkg.com/x/log.v7"
+
+	"github.com/lvzhihao/uchat4mq/libs"
+	"github.com/lvzhihao/uchatlib"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+)
+
+// messageCmd represents the message command
+var messageCmd = &cobra.Command{
+	Use:   "message",
+	Short: "convert uchat message",
+	Long:  `convert uchat message`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// zap logger
+		var logger *zap.Logger
+		if os.Getenv("DEBUG") == "true" {
+			logger, _ = zap.NewDevelopment()
+		} else {
+			logger, _ = zap.NewProduction()
+		}
+		defer logger.Sync()
+
+		consumer, err := libs.NewReceiveConsumer(
+			fmt.Sprintf("amqp://%s:%s@%s/%s", viper.GetString("rabbitmq_user"), viper.GetString("rabbitmq_passwd"), viper.GetString("rabbitmq_host"), viper.GetString("rabbitmq_vhost")),
+			logger,
+		)
+		if err != nil {
+			logger.Fatal("consumer create error", zap.Error(err))
+		}
+		consumer.Consumer("uchat.chat.message", 1, processMessage) //尽量保证聊天记录的时序，以api回调接口收到消息进入receive队列为准
+	},
+}
+
+func processMessage(msg amqp.Delivery, logger *zap.Logger) {
+	logger.Sugar().Info(msg.Body)
+	ret, err := uchatlib.ConvertUchatMessage(msg.Body)
+	logger.Sugar().Fatal(ret, err)
+	if err != nil {
+		msg.Ack(false)
+		logger.Error("process error", zap.Error(err), zap.Any("msg", msg))
+	} else {
+		for _, v := range ret {
+			log.Fatal(v)
+		}
+	}
+}
+
+func init() {
+	RootCmd.AddCommand(messageCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// messageCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// messageCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
---
+++ libs/consumer.go
@@ -0,0 +1,66 @@
+package libs
+
+import (
+	"time"
+
+	"github.com/lvzhihao/goutils"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+)
+
+type receiveConsumer struct {
+	amqpUrl string
+	conn    *amqp.Connection
+	logger  *zap.Logger
+}
+
+func NewReceiveConsumer(url string, logger *zap.Logger) (*receiveConsumer, error) {
+	c := &receiveConsumer{
+		amqpUrl: url,
+		logger:  logger,
+	}
+	// first test dial
+	_, err := amqp.Dial(url)
+	return c, err
+}
+
+func (c *receiveConsumer) link(queue string, prefetchCount int) (<-chan amqp.Delivery, error) {
+	var err error
+	c.conn, err = amqp.Dial(c.amqpUrl)
+	if err != nil {
+		c.logger.Error("amqp.open", zap.Error(err))
+		return nil, err
+	}
+	_, err = c.conn.Channel()
+	if err != nil {
+		c.logger.Error("channel.open", zap.Error(err))
+		return nil, err
+	}
+	channel, _ := c.conn.Channel()
+	if err := channel.Qos(prefetchCount, 0, false); err != nil {
+		c.logger.Error("channel.qos", zap.Error(err))
+		return nil, err
+	}
+	deliveries, err := channel.Consume(queue, "ctag-"+goutils.RandomString(20), false, false, false, false, nil)
+	if err != nil {
+		c.logger.Error("base.consume", zap.Error(err))
+		return deliveries, err
+	}
+	return deliveries, nil
+}
+
+func (c *receiveConsumer) Consumer(queue string, prefetchCount int, handle func(msg amqp.Delivery, logger *zap.Logger)) {
+	for {
+		time.Sleep(3 * time.Second)
+		deliveries, err := c.link(queue, prefetchCount)
+		if err != nil {
+			c.logger.Error("Consumer Link Error", zap.Error(err))
+			continue
+		}
+		for msg := range deliveries {
+			go handle(msg, c.logger)
+		}
+		c.conn.Close()
+		c.logger.Info("Consumer ReConnection After 3 Second")
+	}
+}
